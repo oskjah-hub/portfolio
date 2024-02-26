8f6c278aee878375f40b6d23b9d9e1217632e683
Oskari Jahkola <jahkola.oskari@gmail.com>
2024-02-26 13:47:13 +0200

    NavLink and inital home page look

---
+++ src/app/page.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* IMAGE CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image src="/hero.png" alt="" fill className="object-contain" />
+      </div>
+      {/* TEXT CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        {/* TITLE */}
+        <h1 className="text-4xl font-bold md:text-6xl">
+          Crafting Digital Experiences, Designing Tomorrow.
+        </h1>
+        {/* DESC */}
+        <p className="md:text-xl">
+          Welcome to my digital canvas, where innocation and creativity
+          converge. With a keen eye for aesthetics and performance, I thrive on
+          bringing the best of both worlds.
+        </p>
+        {/* BUTTONS */}
+        <div className="w-full flex gap-4">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            View My Work
+          </button>
+          <button className="p-4 rounded-lg ring-1 ring-black bg-white text-black">
+            Contact Me
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import NavLink from "./NavLink";
 
 const links = [
   { url: "/", title: "Home" },
@@ -13,17 +14,15 @@ const links = [
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LINKS */}
       <div className="hidden md:flex gap-4 w-1/3">
         {links.map((link) => (
-          <Link href={link.url} key={link.title}>
-            {link.title}
-          </Link>
+          <NavLink link={link} key={link.title} />
         ))}
       </div>
       {/* LOGO */}
-      <div className="md:hidden lg:flex w-1/3 justify-center">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
