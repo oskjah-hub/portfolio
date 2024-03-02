bf2b1365f319d33f1a6ffea4bef10a156fe05426
Oskari Jahkola <jahkola.oskari@gmail.com>
2024-03-02 16:19:47 +0200

    Initial experience logic into about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,7 +9,7 @@ const AboutPage = () => {
       transition={{ duration: 1 }}
     >
       {/* CONTAINTER */}
-      <div className="">
+      <div className="h-full overflow-scroll">
         {/* TEXT CONTAINER */}
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
           {/* BIO CONTAINER */}
@@ -28,9 +28,130 @@ const AboutPage = () => {
             </div>
           </div>
           {/* SKILLS CONTAINTER */}
-          <div className="">skills</div>
+          <div className="flex flex-col gap-12 justify-center">
+            {/* SKILLS TITLE */}
+            <h1 className="font-bold text-2xl">SKILLS</h1>
+            {/* SKILLS LIST */}
+            <div className="flex flex-wrap gap-4">
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                JavaScript
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                React
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                NextJS
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Python
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                TailwindCss
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                TypeScript
+              </div>
+            </div>
+            {/* SKILLS SCROLL SVG */}
+          </div>
           {/* EXPERIENCE CONTAINER */}
-          <div className="">exp</div>
+          <div className="flex flex-col gap-12 justify-center pb-48">
+            {/* EXP TITLE */}
+            <h1 className="font-bold text-2xl">EXPERIENCE</h1>
+            {/* EXPERIENCE LIST */}
+            <div className="">
+              {/* EXPERIENCE LIST ITEM 1 */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior JavaScript Engineer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">Current employment</div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2019 - Present
+                  </div>
+                  {/* COMPANY NAME */}
+                  <div className="bg-white rounded text-sm text-black font-semibold w-fit">
+                    Smartly.io
+                  </div>
+                </div>
+                {/* CENTER */}
+                <div className="w-1/6">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* JOB NODE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-500 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3"></div>
+              </div>
+              {/* EXPERIENCE LIST ITEM 2 */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3"></div>
+                {/* CENTER */}
+                <div className="w-1/6">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* JOB NODE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-500 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior JavaScript Engineer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">Current employment</div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2019 - Present
+                  </div>
+                  {/* COMPANY NAME */}
+                  <div className="bg-white rounded text-sm text-black font-semibold w-fit">
+                    Smartly.io
+                  </div>
+                </div>
+              </div>
+              {/* EXPERIENCE LIST ITEM 2 */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior JavaScript Engineer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">Current employment</div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2019 - Present
+                  </div>
+                  {/* COMPANY NAME */}
+                  <div className="bg-white rounded text-sm text-black font-semibold w-fit">
+                    Smartly.io
+                  </div>
+                </div>
+                {/* CENTER */}
+                <div className="w-1/6">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* JOB NODE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-500 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3"></div>
+              </div>
+            </div>
+          </div>
         </div>
         {/* SVG CONTAINTER */}
         <div className=""></div>
